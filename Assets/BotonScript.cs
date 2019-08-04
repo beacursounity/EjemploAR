7ab2fc80aea6005047a4f4f87740e814640675fd
beacursounity <43536093+beacursounity@users.noreply.github.com>
2019-08-04 18:05:38 +0200

    Initial commit

---
+++ Assets/BotonScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonScript : MonoBehaviour {
+
+    // REFERNCIA ANIMATOR PARA CAMBIARLE EL BOOLEANO
+    public Animator  animatorHamburguesa;
+    private const string PARAM_ANIMADO = "animado";
+
+	public void Animar() {
+        animatorHamburguesa.SetBool(PARAM_ANIMADO, true);
+    }
+}
